92d838a586722e2a120aa77cbb74beccf9a26195
Joel Cretan <jcretan@gmail.com>
2017-08-24 23:10:56 -0700

    able to edit location list
    can delete locations by long pressing and using the popup menu

--- app/src/main/java/com/laserscorpion/redadalertas/LocationListActivity.java
+++ app/src/main/java/com/laserscorpion/redadalertas/LocationListActivity.java
@@ -9,8 +9,11 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -23,12 +26,15 @@ import com.laserscorpion.redadalertas.db.LocationPrefDatabaseHelper;
 import com.laserscorpion.redadalertas.db.StoredLocation;
 
 import java.util.ArrayList;
+
 import com.google.android.gms.location.places.ui.PlacePicker;
 
 
 public class LocationListActivity extends AppCompatActivity {
     private static final String TAG = "LocationListActivity";
     private static final int PLACE_PICKER_REQUEST = 22919; // some arbitrary number I chose
+    private static final int KEEP_MENU_INDEX = 0;
+    private static final int DELETE_MENU_INDEX = 1;
     private Activity activity = this;
     private LocationPrefDatabaseHelper db;
     private ArrayAdapter<StoredLocation> adapter;
@@ -47,16 +53,18 @@ public class LocationListActivity extends AppCompatActivity {
         adapter = new ArrayAdapter<StoredLocation>(this, R.layout.location_list_item, savedLocations);
         ListView list = (ListView)findViewById(R.id.location_listview);
         list.setAdapter(adapter);
+        registerForContextMenu(list);
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             list.setNestedScrollingEnabled(true);
-        }
+        }*/
     }
 
     @Override
     protected void onResume() {
         super.onResume();
         updateList();
+
     }
 
     @Override
@@ -67,13 +75,49 @@ public class LocationListActivity extends AppCompatActivity {
     }
 
     private void updateList() {
-        //savedLocations = db.getLocations();
         ArrayList<StoredLocation> currentLocations = db.getLocations();
-        ArrayList<StoredLocation> newLocations = new ArrayList<>(currentLocations);
+        if (currentLocations.equals(savedLocations))
+            return;
+        for (StoredLocation location : savedLocations) {
+            if (!currentLocations.contains(location))
+                adapter.remove(location);
+        }
+        for (StoredLocation location : currentLocations) {
+            if (!savedLocations.contains(location))
+                adapter.add(location);
+        }
+
+        /*ArrayList<StoredLocation> newLocations = new ArrayList<>(currentLocations);
         newLocations.removeAll(savedLocations);
-        adapter.addAll(newLocations);
+        adapter.addAll(newLocations);*/
         adapter.notifyDataSetChanged();
+
         savedLocations = currentLocations;
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo info) {
+        if (v.getId() == R.id.location_listview) {
+            AdapterView.AdapterContextMenuInfo menuInfo = (AdapterView.AdapterContextMenuInfo)info;
+            menu.setHeaderTitle(savedLocations.get(menuInfo.position).toString());
+            String actionDelete = getString(R.string.location_action_delete_location);
+            String actionKeep = getString(R.string.location_action_keep_location);
+            menu.add(Menu.NONE, KEEP_MENU_INDEX, Menu.NONE, actionKeep);
+            menu.add(Menu.NONE, DELETE_MENU_INDEX, Menu.NONE, actionDelete);
+        }
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        if (item.getItemId() == DELETE_MENU_INDEX) {
+            AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)item.getMenuInfo();
+            StoredLocation location = savedLocations.get(info.position);
+            db.deleteLocation(location.ID);
+            //savedLocations.remove(location);
+            //adapter.notifyDataSetChanged();
+            updateList();
+        }
+        return true;
     }
 
     @Override
--- app/src/main/java/com/laserscorpion/redadalertas/db/StoredLocation.java
+++ app/src/main/java/com/laserscorpion/redadalertas/db/StoredLocation.java
@@ -3,11 +3,25 @@ package com.laserscorpion.redadalertas.db;
 import android.location.Location;
 
 public class StoredLocation {
-    int ID;
-    Location location;
+    public int ID;
+    public Location location;
 
     @Override
     public String toString() {
+        // todo: this should be translatable
         return location.toString();
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if ( !(obj instanceof StoredLocation) )
+            return false;
+        StoredLocation other = (StoredLocation)obj;
+        Location otherLoc = other.location;
+        if (ID != other.ID)
+            return false;
+        return otherLoc.getLongitude() == location.getLongitude() && otherLoc.getLatitude() == location.getLatitude();
+    }
+
+    //todo: implement HashCode() too
 }
